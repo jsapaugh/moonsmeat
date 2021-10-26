8bed0a0b84a498bce34f062085a202391b855c65
Jacob Sapaugh <jsapaugh@gmail.com>
2021-10-25 22:42:04 -0500

    working on github pages

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,7 +7,7 @@ import { Link } from 'react-router-dom'
 const Home = ({ props }) => {
   return (
     <div>
-      <Link to="/" />
+      <Link to="/home" />
       <h1>Welcome to Moon's Meat</h1>
       <img alt="Moon Man" src="./MoonMan.jpg" style={{width: 400}}/>
     </div>
